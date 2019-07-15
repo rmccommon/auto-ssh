a0148235d4800606d9b79e189780d7b2e1be5625
Scott Birch <sbirch@symboticware.com>
2019-07-15 15:28:47 -0700

    Adjusted list feature to no longer be required

--- auto-ssh_in_d3v.py
+++ auto-ssh_in_d3v.py
@@ -21,7 +21,7 @@ USERNAME = "rmccommon"
 parser = argparse.ArgumentParser(description='Tool to help assist with auto ssh comands.')
 parser.add_argument('-s', help= 'Enable or disable super user elevation when using comands.', default=False, action='store_true')
 parser.add_argument('-ip', help= 'String value of ip address to connect to.', type=str, required='True')
-parser.add_argument('-f', help= 'Add files for transfer TO ssh target ip.', nargs='+', required='False')
+parser.add_argument('-f', help= 'Add files for transfer TO ssh target ip.', nargs='+')
 args = parser.parse_args()
 
 
